ff721c6b595f2df53981614b64cd93aad4d1dbca
James Bridges <masterbridges@gmail.com>
2020-01-03 23:23:53 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+  def name
+    @name
+  end
+
+  def name=(dog_name)
+    @name = dog_name
+  end
+
+  def breed
+    @name
+  end
+
+  def breed=(breednam)
+    @breed = breednam
+  end
+end
